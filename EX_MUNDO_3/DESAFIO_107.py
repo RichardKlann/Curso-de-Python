b14bd2c9170886f76c3270e6f3aed918ef32488a
RichardKlann <richardarielklann34@gmail.com>
2026-05-27 21:51:23 -0300

    Finished the theory of class 22, and get all the exercises to do.

---
+++ EX_MUNDO_3/DESAFIO_107.py
@@ -0,0 +1,5 @@
+'''
+Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobrar(), e metade().
+
+Faça também um programa que importe esse módulo e use algumas dessas funções.
+'''
