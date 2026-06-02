7fecdfddd533b69a6b9746a6d623ae761b929411
RichardKlann <richardarielklann34@gmail.com>
2026-06-01 21:56:46 -0300

    Exercise n°111 solved

--- EX_MUNDO_3/DESAFIO_111/teste.py
+++ EX_MUNDO_3/DESAFIO_111/teste.py
@@ -3,3 +3,9 @@ Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados m
 
 Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
 '''
+
+from utilidadecev import moeda
+
+i = input('Digite o preço: ').replace(',','.')
+p = float(i)
+moeda.resumo(p, 30, 10)
