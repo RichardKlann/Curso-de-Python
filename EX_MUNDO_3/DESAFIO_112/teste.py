aa4a2086cebecd0b65ad4de00bdc2afd594174e7
RichardKlann <richardarielklann34@gmail.com>
2026-06-01 22:21:53 -0300

    Exercise n°112 solved

--- EX_MUNDO_3/DESAFIO_112/teste.py
+++ EX_MUNDO_3/DESAFIO_112/teste.py
@@ -1,3 +1,10 @@
 '''
 Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função input mas com uma validação de dados para aceitar apenas valores que sejam monetários.
 '''
+
+from utilidadecev import moeda
+from utilidadecev import dado 
+
+i = dado.leiaDinheiro('Digite o preço: R$')
+p = float(i)
+moeda.resumo(p, 30, 10)
