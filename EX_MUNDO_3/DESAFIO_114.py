4d89d6a3a4e4f16d8b69c61be84269ebfc92f43d
RichardKlann <richardarielklann34@gmail.com>
2026-06-03 21:29:19 -0300

    Exercise n°114 solved

--- EX_MUNDO_3/DESAFIO_114.py
+++ EX_MUNDO_3/DESAFIO_114.py
@@ -2,3 +2,19 @@
 Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.
 '''
 
+import urllib.request
+import urllib.error
+
+url = "https://www.goog.com.br"
+
+try:
+    urllib.request.urlopen(url, timeout=5)
+
+except urllib.error.HTTPError as e:
+    print(f"Servidor respondeu (HTTP {e.code})")
+
+except urllib.error.URLError:
+    print("Servidor inacessível")
+
+else:
+    print("Servidor respondeu (HTTP 200)")
