9b866813a269e607c98cdf60c89ed69bce3970f4
RichardKlann <richardarielklann34@gmail.com>
2026-06-10 22:10:52 -0300

    Starting study OOP

---
+++ _MUNDO 4.py
@@ -0,0 +1,28 @@
+#PROGRAMAÇÃO EM PYTHON COM ORIENTAÇÃO A OBJETOS
+'''
+BIBLIOGRAFIA RECOMENDADA
+- Introdução à Programação com Python - Nilo Ney Coutinho Menezes
+- Object-Oriented Python - Master OOP by Building Games and GUIs - IRV KALB
+- Python 3 the Comprehensive Guide - Johanes Ernest
+'''
+
+#FUNDAMENTOS
+'''
+DE ONDE VEM A PROGRAMAÇÃO ORIENTADA A OBJETOS?
+
+POO - Programação Orientada a Objetos
+OOP - Object Oriented Programming
+
+"Representar elementos do mundo real nos sistemas computacionais"
+
+"Crise do Software" de 1960?
+
+1950 - Início das linguagens de baixo nível: assembly
+1955 - Linguagens lineares - Fortran, Cobol, C, Basic
+1960 - Linguagens estruturadas
+1965 - Linguagens modulares
+1970 - Linguagens orientadas a objetos
+
+OOAD - Object Oriented Analysis and Design
+UML - Unified Modeling Language
+'''
