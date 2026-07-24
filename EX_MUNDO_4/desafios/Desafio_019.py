7f6f9c6ebe21b228e4df071040418f860ed5773a
RichardKlann <richardarielklann34@gmail.com>
2026-07-23 21:45:16 -0300

    Exercise n° 019 improvement

--- EX_MUNDO_4/desafios/Desafio_019.py
+++ EX_MUNDO_4/desafios/Desafio_019.py
@@ -7,48 +7,44 @@ from rich import inspect
 from time import sleep
 
 class Livro:
-
-    def __init__(self, titulo, qtd_pag, pagina_atual=1): #Método construtor
+    def __init__(self, titulo, paginas): #Método construtor
         #Atributos de instância
         self.titulo = titulo
-        self.qtd_pag = qtd_pag
-        self.pagina_atual = pagina_atual
-        print(f"Você acabou de abrir o livro '[red]{self.titulo}[/]' que possui [green]{self.qtd_pag} páginas[/] no total.", end=" ")
-        print(f"Você agora está na [yellow]página n° {self.pagina_atual}[/].")
+        self.total_paginas = paginas
+        self.pagina_atual = 1
 
-    def __str__(self) -> str:
-        return f"Título: {self.titulo}\nQuantidade de páginas {self.qtd_pag}"
+        print(f":open_book: [blue]Você acabou de abrir o livro [red]{self.titulo}[/] que possui {self.total_paginas}", end="")
+        print(f" [blue]páginas no total. Você agora está na [yellow]página {self.pagina_atual}")
 
     #Métodos de instância
-    def avancar_paginas(self, qtd_pag_avanca=0):
-            if (qtd_pag_avanca + self.pagina_atual) > self.qtd_pag:
-                qtd_pag_avanca = self.qtd_pag
-            cont = 1
-            while cont <= qtd_pag_avanca:
-                if self.pagina_atual != self.qtd_pag:
-                    self.pagina_atual += 1
-                    print(f"Pág.{self.pagina_atual}", end="")
-                    if cont != qtd_pag_avanca:
-                        print(" -> ", end="")
-                        sleep(0.5)
-                    else:
-                        print(f"\nVocê avançou {qtd_pag_avanca} páginas e agora está na página {self.pagina_atual}.")
-                    cont += 1
-                    if self.pagina_atual == self.qtd_pag:
-                        print("Você chegou ao final do livro!")
-                         
-                else:
-                     print("Você chegou ao final do livro!")
-                     break
-                     
+    def avancar_paginas(self, qtd = 1):
+        cont = 0
+        for pg in range(0, qtd, 1):
+            if not self.fim_do_livro():
+                self.pagina_atual += 1
+                print(f"Pág{self.pagina_atual} :arrow_forward:", end=" ")
+                sleep(0.2)
+                cont += 1
+        print(f"[blue]Você avançou {cont} páginas e agora está na [yellow]página {self.pagina_atual}[/][/blue]")
+        if self.fim_do_livro():
+            print(f':closed_book: [red]Você chegou a o final do livro "{self.titulo}"[/red]')
+
+    def fim_do_livro(self) -> bool:
+        if self.pagina_atual == self.total_paginas:
+            return True
+        else:
+            return False
+        "return True if self.pagina_atual == self.total_paginas else False"
+
+
+        
 
 
 
 
+l1 = Livro("10 coisas que aprendi na vida", 20)
 
-l1 = Livro("10 coisas que aprendi na vida", 30)
 
-
-l1.avancar_paginas(18)
+l1.avancar_paginas(5)
 l1.avancar_paginas(10)
-l1.avancar_paginas(4)
+l1.avancar_paginas(20)
